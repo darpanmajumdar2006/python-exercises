c7d00fb4cce6da13cff589282cc07b8c0cbed07d
Darpan Majumdar <darpanm2020@gmail.com>
2025-06-20 23:48:02 +0530

    Add files via upload
    A student is punished to write "I will never spam my friends again." The program should number each sentence and it should make 8 different random looking typos

---
+++ ex2.py
@@ -0,0 +1,52 @@
+i=1
+while i<=8:
+   print(i,".","I will never spam my friends again.")
+   i+=1
+   if i==8:
+        print(i,".","I will never spam my friends again.".upper())
+        i+=1
+while i<=16:
+       print(i,".","I will never spam my friends again.")
+       i+=1
+       if i==16:
+        print(i,".","I".lower(), "will never spam my friends again.")
+        i+=1 
+while i<=24:
+       print(i,".","I will never spam my friends again.")
+       i+=1
+       if i==24:
+        print(i,".","I 3ill never spam my friends again.")
+        i+=1 
+while i<=32:
+       print(i,".","I will never spam my friends again.")
+       i+=1
+       if i==32:
+        print(i,".","I will never my friends again.")
+        i+=1
+while i<=40:
+       print(i,".","I will never spam my friends again.")
+       i+=1
+       if i==40:
+        print(i,".","I will neVer spam my friends again.")
+        i+=1
+while i<=48:
+       print(i,".","I will never spam my friends again.")
+       i+=1
+       if i==48:
+        print(i,".","will never spam my friends again.")
+        i+=1
+while i<=56:
+       print(i,".","I will never spam my friends again.")
+       i+=1
+       if i==56:
+        print(i,".","I will never spam my friend again.")
+        i+=1
+while i<=64:
+       print(i,".","I will never spam my friends again.")
+       i+=1
+       if i==64:
+        print(i,".","I ill never spam my friends again.")
+        i+=1
+for i in range(65, 101):
+         print(i,".","I will never spam my friends again.")
+         
